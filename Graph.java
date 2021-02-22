7085b31b694c85d1ca721e0fce2151891094ba7e
YOUPIDOK <leo.stevenot@gmail.com>
2021-02-22 14:01:18 +0100

    Create squelette

---
+++ Graph.java
@@ -0,0 +1,29 @@
+public class Graph {
+
+    public static void main(String[] args){
+    
+    }
+
+    //Question 1
+    public static void readGraph() {
+
+    }
+
+    public static int[][] generateGraph() {
+        
+        return null;
+    }
+
+    //Question 2
+    public static boolean isConnect(int[][] graph){
+        
+        return false;
+    }
+
+    //Question 3
+    public static int[][] dijkstra(){
+
+        return null;
+    }
+
+}
