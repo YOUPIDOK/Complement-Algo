d397b83a03418d7281760d583ed81cec55c81d10
SuperCope <robin@merome.net>
2021-02-22 14:57:47 +0100

    u

--- Graph.java
+++ Graph.java
@@ -1,17 +1,40 @@
+import java.lang.Math;
+import java.util.Scanner;
+
 public class Graph {
 
     public static void main(String[] args){
-    
+        printGraph(generateGraph(10));
+        readGraph();
     }
 
     //Question 1
     public static void readGraph() {
-
+        Scanner c = new Scanner(System.in);
+        while(c.hasNextLine()){
+            System.out.println(c.nextLine());
+        }
     }
 
-    public static int[][] generateGraph() {
-        
-        return null;
+    public static int[][] generateGraph(int size) {
+        int[][] graph = new int[size][size];
+
+        for(int i=0; i<size; i++){
+            for(int j=0; j<size; j++){
+                graph[i][j] = (int) (Math.random()*9);
+            }    
+        }
+
+        return graph;
+    }
+
+    public static void printGraph(int[][] graph){
+        for(int i=0; i<graph.length; i++){
+            for(int j=0; j<graph.length; j++){
+                System.out.print(graph[i][j] + " | ");
+            }
+            System.out.println("");
+        }
     }
 
     //Question 2
