7d9d457db0cba941e5c7ab615b13785498f12ec7
SuperCope <robin@merome.net>
2021-02-23 10:12:02 +0100

    hsbchfbsh

--- Graph.java
+++ Graph.java
@@ -14,6 +14,12 @@ public class Graph {
 
         System.out.println("\nGraph aléatoire : ");
         System.out.println("IsConnect : " + isConnect(generateGraph(10)));
+
+        System.out.println("\nPlus court chemin : ");
+        int[][] distance = plusCourtChemin(readGraph("Dijkstra.txt"));
+        System.out.println("Distance : ");
+        printGraph(distance);
+
     }
 
     // Question 1
@@ -91,9 +97,89 @@ public class Graph {
     }
 
     // Question 3
-    public static int[][] dijkstra(int[][] graph) {
-        int[][] distance = new int[graph.length][graph.length];
+    // public static int[][] plusCourtChemin2(int[][] graph) {
+    //     if(!isConnect(graph)) return null;
+    //     int size = graph.length;
+    //     int[][] distance = new int[graph.length][graph.length];
 
+    //     for (int i = 0; i < size; i++) {
+    //         for (int j = size - 1; j > i; j--) {
+    //             int dist = plusCourtCheminEntreDeuxNoeuds(i, j, graph);
+    //             distance[i][j] = dist;
+    //             distance[j][i] = dist;
+    //         }
+    //     }
+
+    //     return distance;
+    // }
+
+    public static int plusCourteAretePartantDunNoeud(int sommetRef,int[][] graph){
+        int distMin = 0;
+        for(int i = 0;i<graph.length;i++){
+            if(graph[sommetRef][i]!=0 && (distMin == 0 || graph[sommetRef][i]<distMin)){
+                distMin = graph[sommetRef][i];
+            }
+        }
+        return distMin;
+    }
+    
+    // public static int[] getValsCheminsEntreDeuxNoeud(int sommetRef,int sommetDest,int[][] graph){
+    //     int sommetRefTemp = sommetRef;
+    //     ArrayList noeudsAnalyses = new ArrayList();
+    //     ArrayList noeudsAAnalyser = new ArrayList();
+    //     noeudsAAnalyser.add(sommetRef);
+    //     int i = 0; 
+    //     while(!noeudsAAnalyser.isEmpty()){
+    //         if(graph[sommetRefTemp][i]!=0 && !noeudsAnalyses.contains(i)){   
+    //             noeudsAAnalyser.add(i);
+    //         }
+    //         if(i==graph.length-1){
+    //             i = 0;
+    //             noeudsAAnalyser.remove(sommetRefTemp);
+    //             noeudsAnalyses.add(i);
+    //         }
+    //     
+    //     i++;
+    //     }
+
+    // }
+    
+
+    public static int[][] plusCourtChemin(int[][] graph){
+        if(!isConnect(graph)) return null;
+        int[][] distance = new int[graph.length][graph.length];
+        for(int i=0; i<graph.length; i++){
+            int[] line = dijkstra(graph, i);
+            for(int j=0; j<graph.length; j++){
+                distance[i][j] = line[j];
+            }
+        }
+        
         return distance;
     }
-}
+
+    public static int[] dijkstra(int[][] graph,int sommet){
+        int[] line = new int[graph.length];
+        Set<Integer> visite = new HashSet<Integer>();
+
+        //1ere Etape
+        line = graph[sommet];
+        visite.add(sommet);
+
+        for(int i=0; i<graph.length-1; i++){
+            int sommet = choiceSommet(line, visite);
+        }
+
+        return line;
+    }
+
+    public static int choiceSommet(int[] line,Set<Integer> viste){
+        int min = 0;
+        int index;
+
+        for(int i : line){
+            if(!line.contains(i) && )            
+        }
+    }
+    
+}   
