95b68e9e7b97bf5e86fb103724990da36227c6b2
SuperCope <robin@merome.net>
2021-02-25 08:47:14 +0100

    Djikstra Fonctionne!

--- Graph.java
+++ Graph.java
@@ -15,10 +15,12 @@ public class Graph {
         System.out.println("\nGraph aléatoire : ");
         System.out.println("IsConnect : " + isConnect(generateGraph(10)));
 
-        System.out.println("\nPlus court chemin : ");
-        int[][] distance = plusCourtChemin(readGraph("Dijkstra.txt"));
-        System.out.println("Distance : ");
-        printGraph(distance);
+        System.out.println("\nDjikstra : ");
+        System.out.println("IsConnect : " + isConnect(readGraph("Graph2.txt")));
+        System.out.println(dijkstra(readGraph("Graph2.txt"), 0, readGraph("Graph2.txt").length - 1));
+        // int[][] distance = plusCourtChemin(readGraph("Dijkstra.txt"));
+        // System.out.println("Distance : ");
+        // printGraph(distance);
 
     }
 
@@ -96,90 +98,149 @@ public class Graph {
         return false;
     }
 
-    // Question 3
-    // public static int[][] plusCourtChemin2(int[][] graph) {
-    //     if(!isConnect(graph)) return null;
-    //     int size = graph.length;
-    //     int[][] distance = new int[graph.length][graph.length];
+    // if(noeudsReperes.size()==1)
 
-    //     for (int i = 0; i < size; i++) {
-    //         for (int j = size - 1; j > i; j--) {
-    //             int dist = plusCourtCheminEntreDeuxNoeuds(i, j, graph);
-    //             distance[i][j] = dist;
-    //             distance[j][i] = dist;
-    //         }
-    //     }
-
-    //     return distance;
+    // {
+    // // On a que le noeud de départ
+    // sommetRef = depart;
+    // }else
+    // {
+    // // On choisit parmis les valeurs non marquées la plus petite
+    // sommetRef = Collections.min(noeudsReperes);
     // }
 
-    public static int plusCourteAretePartantDunNoeud(int sommetRef,int[][] graph){
-        int distMin = 0;
-        for(int i = 0;i<graph.length;i++){
-            if(graph[sommetRef][i]!=0 && (distMin == 0 || graph[sommetRef][i]<distMin)){
-                distMin = graph[sommetRef][i];
+    public static int dijkstra(int[][] graph, int depart, int arrivee) {
+        ArrayList<Integer> noeudsMarques = new ArrayList<Integer>();
+        ArrayList<Integer> noeudsReperes = new ArrayList<Integer>();
+        ArrayList<Integer> valsNoeudsReperes = new ArrayList<Integer>();
+        ArrayList<Integer> longueursChemin = new ArrayList();
+        valsNoeudsReperes.add(graph[depart][depart]);
+        noeudsReperes.add(depart);
+        int valSommetRef = -1;
+        int sommetRef = -1;
+        while (noeudsMarques.size() != graph.length - 1) {
+
+            valSommetRef = Collections.min(valsNoeudsReperes);
+            sommetRef = noeudsReperes.get(valsNoeudsReperes.indexOf(valSommetRef));
+            for (int sommetDest = 0; sommetDest < graph[sommetRef].length; sommetDest++) {
+                if (graph[sommetRef][sommetDest] != 0 && !noeudsMarques.contains(sommetDest)) {
+                    System.out.println("->" + sommetRef + "," + graph[sommetRef][sommetDest] + "," + sommetDest);
+                    if (noeudsReperes.indexOf(sommetDest) >= 0
+                            && valsNoeudsReperes.get(noeudsReperes.indexOf(sommetDest)) >= 0) {
+                        valsNoeudsReperes.set(noeudsReperes.indexOf(sommetDest),
+                                Math.min(valsNoeudsReperes.get(noeudsReperes.indexOf(sommetDest)),
+                                        valSommetRef + graph[sommetRef][sommetDest]));
+                    } else {
+                        if (sommetDest == arrivee) {
+                            System.out.println("AJOUT" + (valSommetRef + graph[sommetRef][sommetDest]));
+                            longueursChemin.add(valSommetRef + graph[sommetRef][sommetDest]);
+                        } else {
+                            noeudsReperes.add(sommetDest);
+                            valsNoeudsReperes.add(valSommetRef + graph[sommetRef][sommetDest]);
+                        }
+
+                    }
+
+                }
+
             }
+
+            valsNoeudsReperes.remove(noeudsReperes.indexOf(sommetRef));
+            noeudsReperes.remove(noeudsReperes.indexOf(sommetRef));
+            noeudsMarques.add(sommetRef);
         }
-        return distMin;
+
+        return Collections.min(longueursChemin);
     }
-    
-    // public static int[] getValsCheminsEntreDeuxNoeud(int sommetRef,int sommetDest,int[][] graph){
-    //     int sommetRefTemp = sommetRef;
-    //     ArrayList noeudsAnalyses = new ArrayList();
-    //     ArrayList noeudsAAnalyser = new ArrayList();
-    //     noeudsAAnalyser.add(sommetRef);
-    //     int i = 0; 
-    //     while(!noeudsAAnalyser.isEmpty()){
-    //         if(graph[sommetRefTemp][i]!=0 && !noeudsAnalyses.contains(i)){   
-    //             noeudsAAnalyser.add(i);
-    //         }
-    //         if(i==graph.length-1){
-    //             i = 0;
-    //             noeudsAAnalyser.remove(sommetRefTemp);
-    //             noeudsAnalyses.add(i);
-    //         }
-    //     
-    //     i++;
-    //     }
+    // int size = graph.length;
+    // int[][] distance = new i
+
+    // for (int j = size - 1; j > i; j-
+    // int dist = plusCourtCheminEntreDeuxN
+    // distance[i][j] = dist;
+    // distance[j][i] = dist;
+    // }
+    // }
 
     // }
-    
 
-    public static int[][] plusCourtChemin(int[][] graph){
-        if(!isConnect(graph)) return null;
-        int[][] distance = new int[graph.length][graph.length];
-        for(int i=0; i<graph.length; i++){
-            int[] line = dijkstra(graph, i);
-            for(int j=0; j<graph.length; j++){
-                distance[i][j] = line[j];
-            }
-        }
-        
-        return distance;
-    }
+    // graph){
+    // int dis
+    // for(int i = 0;i<
+    // if(graph[sommetRef][i]!=0 && (dist
+    // distMin = graph[sommetRef][i];
+    // }
+    // }
+    // r
+    // }
 
-    public static int[] dijkstra(int[][] graph,int sommet){
-        int[] line = new int[graph.length];
-        Set<Integer> visite = new HashSet<Integer>();
+    // sommetDest,int[][] graph){
+    // int sommetRefTemp = sommet
+    // ArrayList<Integer> noeudsAnaly
+    // ArrayList<Integer> noeudsAAnalyser = new ArrayList<Integer>()
+    // ArrayList<Integer> valsChemins = new ArrayList();
+    // int valChemin = 0;
+    // noeudsAAnalyser.ad
+    // int i = 0;
+    // boolean co
+    // while(!noeudsAAnalyser.i
 
-        //1ere Etape
-        line = graph[sommet];
-        visite.add(sommet);
+    // valsChemins.add(valChemin);
+    // valChemin = valChemin -
+    // graph[sommetRefTemp][no
+    // // noeudsAnalyses.add(sommetRefTemp);
+    // // sommetRefTemp = noeudsAAnalyser.ge
+    // // noeudsAAnalyser.remove(0);
+    // }
+    // i
+    // // On est en train d'analyser un noeud et on découvre une arêt
+    // noeudsAAnalyser.add(i);
+    // if(!counted){
+    // valChemin +=
+    // counted = true;
+    // }
+    // }
+    // i
+    // // On a finit d'analys
+    // i = 0;
+    // noeuds
+    // noeudsAAnalyser.remove(sommetRefTe
+    // sommetRefTemp = noeudsAAnalyser.get(0)
+    // counted = false;
+    // }else{
+    // i++;
+    // }
 
-        for(int i=0; i<graph.length-1; i++){
-            int sommet = choiceSommet(line, visite);
-        }
+    // r
+    // }
 
-        return line;
-    }
+    // if(!isConnect(graph)) return null;
+    // int[][] distance = new int[graph.l
+    // for(int i=0; i<graph.length; i++){
+    // int[] line = dijkstra(graph, i);
+    // for(int j=0; j<graph.length; j++
+    // distance[i][j] = line[j];
+    // }
+    // }
 
-    public static int choiceSommet(int[] line,Set<Integer> viste){
-        int min = 0;
-        int index;
+    // }
 
-        for(int i : line){
-            if(!line.contains(i) && )            
-        }
-    }
-    
-}   
+    // int[] line = new int[graph.length];
+    // Set<Integer> visite = new HashSet<I
+
+    // line = graph
+    // visite.add(sommet);
+
+    // int sommet = choiceSommet(line, visi
+    // }
+
+    // }
+
+    // int min = 0;
+    // int index;
+
+    // if(!line.contains(
+    // }
+    // }
+
+}
